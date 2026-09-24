7fe264b525a20726fb296fd6167a05f774e19d08
DOCTA S.D.I. <docta.sdi@gmail.com>
2026-09-24 17:44:02 -0300

    Prueba
    Prueba realizada el miércoles 23/09/2026 en el Club, de noche.

---
+++ data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pano0001---pano0034",
+      "name": "PANO0001 - PANO0034",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "VECINOS_PRUEBA1",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
